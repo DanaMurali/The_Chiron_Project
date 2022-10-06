74ecea06e059a9d8d2569efac8b750db9fed8eee
Jonathan Hutchinson <86778615+Djontleman@users.noreply.github.com>
2022-10-06 21:23:56 +0100

    Update config for t3 app (#15)

--- apps/t3-client/tailwind.config.cjs
+++ apps/t3-client/tailwind.config.cjs
@@ -1,6 +1,15 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+const { join } = require('path');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    join(
+      'apps/t3-client',
+      '{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies('apps/t3-client'),
+  ],
   theme: {
     extend: {},
   },
