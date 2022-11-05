a7b2dcf8acd4c11545ec80d10ad0b676a7239fed
Jordan Smith <jelliesmiff@gmail.com>
2022-11-05 12:20:34 +0000

    fix page routing and vh of each page (#36)

--- apps/t3-client/src/components/layout/layout.tsx
+++ apps/t3-client/src/components/layout/layout.tsx
@@ -1,17 +1,17 @@
-import { ReactNode } from "react"
-import Footer from "../footer/footer";
-import Header from "../header/header";
+import { ReactNode } from 'react';
+import Footer from '../footer/footer';
+import Header from '../header/header';
 
 type LayoutProps = {
-    children: ReactNode;
-}
+  children: ReactNode;
+};
 
-export const Layout = ({children}: LayoutProps) => {
-    return (
-        <div className="min-h-full">
-        <Header />
-        <main>{children}</main>
-        <Footer />
-        </div>
-    )
-}
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="min-h-full">
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </div>
+  );
+};
