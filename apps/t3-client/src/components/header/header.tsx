0746395456a2b1f95941f8a2cb684f8bdc486497
JJ <jelliesmiff@gmail.com>
2022-11-08 20:17:53 +0000

    repsonsive navbar

--- apps/t3-client/src/components/header/header.tsx
+++ apps/t3-client/src/components/header/header.tsx
@@ -1,12 +1,79 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import NavMenu from './nav-menu';
+import { useState } from 'react';
 
-const Header = () => {
+type Props = {
+  isLoggedIn?: boolean;
+};
+
+const defaultNavBarData = [
+  {
+    label: 'home',
+    href: '/',
+  },
+  {
+    label: 'events',
+    href: '#mentoring-events',
+  },
+  {
+    label: 'testimonials',
+    href: '#testimonials',
+  },
+  {
+    label: 'contact',
+    href: '#contact',
+  },
+];
+
+const loggedInNavBarData = [
+  {
+    label: 'dashboard',
+    href: '/dashboard',
+  },
+  {
+    label: 'search',
+    href: '/search',
+  },
+  {
+    label: 'logout',
+    href: '/',
+  },
+];
+
+const Header = ({ isLoggedIn }: Props) => {
+  const [isBurgerOpen, setIsBurgerOpen] = useState(false);
+
+  const openMenu = () => {
+    setIsBurgerOpen(true);
+  };
+
+  const closeMenu = () => {
+    setIsBurgerOpen(false);
+  };
+
+  const data = (
+    <>
+      {isLoggedIn
+        ? loggedInNavBarData.map((i, key) => (
+            <li
+              key={key}
+              className="hover:text-champagnePink md:hover:text-hoverTeal list-none"
+            >
+              <a href={i.href}>{i.label}</a>
+            </li>
+          ))
+        : defaultNavBarData.map((i, key) => (
+            <li key={key} className="list-none">
+              <a href={i.href}>{i.label}</a>
+            </li>
+          ))}
+    </>
+  );
+
   return (
     <>
-      <nav className="bg-headerPink relative mx-auto flex max-h-[4rem] items-center justify-between py-2 px-4">
-        <span>
+      <nav className="bg-headerPink relative mx-auto flex h-[4rem] items-center justify-between px-1 pt-2 ">
+        <span className="py-2 pl-4">
           <Link href="/">
             <Image
               src="/assets/chiron-crop.png"
@@ -16,9 +83,33 @@ const Header = () => {
             />
           </Link>
         </span>
-        <span>
-          <NavMenu />
-        </span>
+        <button onClick={openMenu} className={`cursor-pointer md:hidden`}>
+          <Image
+            src="/assets/better-stolen-burger.png"
+            width={50}
+            height={30}
+            alt="burger-menu"
+          />
+        </button>
+        <ul className={`hidden space-x-8 pr-4 md:flex`}>{data}</ul>
+
+        {isBurgerOpen && (
+          <div
+            className={`bg-blackCoral modal fixed top-0 right-0 z-50 min-w-[15rem] max-w-full overflow-visible rounded px-2 py-2`}
+          >
+            <div className="flex justify-end">
+              <button
+                className="close hover:text-grey focus:text-grey float-right mr-4 text-[28px] font-bold text-white hover:cursor-pointer"
+                onClick={() => closeMenu()}
+              >
+                &times;
+              </button>
+            </div>
+            <div className="pl-5 pb-2 text-lg leading-10 text-white">
+              {data}
+            </div>
+          </div>
+        )}
       </nav>
       <div className="bg-blackCoral relative mx-auto p-2"></div>
     </>
