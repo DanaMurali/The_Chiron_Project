61d783f7ee79d1f4ed3d6378a25a4e04b7b2b579
DanaMurali <mdarania@gmail.com>
2022-11-08 20:35:46 +0000

    pulling from main and removing middleware causing issues

--- apps/t3-client/src/server/router/createUser.ts
+++ apps/t3-client/src/server/router/createUser.ts
@@ -1,46 +1,36 @@
-import { TRPCError } from '@trpc/server/dist/declarations/src/TRPCError';
 import { z } from 'zod';
 import { createRouter } from './context';
 
 // Following this tutorial
 // https://dev.to/nexxeln/build-a-full-stack-app-with-create-t3-app-5e1e
 
-export const createUser = createRouter()
-  .middleware(async ({ ctx, next }) => {
-    // Any queries or mutations after this middleware will
-    // raise an error unless there is a current session
-    if (!ctx.session) {
-      throw new TRPCError({ code: 'UNAUTHORIZED' });
+export const createUser = createRouter().mutation('createUser', {
+  input: z.object({
+    name: z.string(),
+    email: z.string(),
+    image: z.string(),
+    career: z.string(),
+    jobRole: z.string(),
+    biography: z.string(),
+    isMentor: z.boolean(),
+    isMentee: z.boolean(),
+  }),
+  async resolve({ ctx, input }) {
+    try {
+      await ctx.prisma.user.create({
+        data: {
+          name: input.name,
+          email: input.email,
+          image: input.image,
+          career: input.career,
+          jobRole: input.jobRole,
+          biography: input.biography,
+          isMentor: input.isMentor,
+          isMentee: input.isMentee,
+        },
+      });
+    } catch (error) {
+      console.log(error);
     }
-    return next();
-  })
-  .mutation('createUser', {
-    input: z.object({
-      name: z.string(),
-      email: z.string(),
-      image: z.string(),
-      career: z.string(),
-      jobRole: z.string(),
-      biography: z.string(),
-      isMentor: z.boolean(),
-      isMentee: z.boolean(),
-    }),
-    async resolve({ ctx, input }) {
-      try {
-        await ctx.prisma.user.create({
-          data: {
-            name: input.name,
-            email: input.email,
-            image: input.image,
-            career: input.career,
-            jobRole: input.jobRole,
-            biography: input.biography,
-            isMentor: input.isMentor,
-            isMentee: input.isMentee,
-          },
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  });
+  },
+});
