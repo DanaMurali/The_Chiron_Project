88c3c5dd86c27afe56bb4795ddcd2e1e2aec19e8
Rose Tanner <91726594+roset99@users.noreply.github.com>
2022-11-09 17:57:36 +0000

    Requests modal (#58)
    * mentor request modal
    
    * request modal

---
+++ apps/t3-client/src/components/requests/requests.tsx
@@ -0,0 +1,44 @@
+import ProfileCard from '../search-page/profile-card/profile-card';
+import Image from 'next/image';
+
+type Props = {
+  requestNumber: number;
+  onClose: () => void;
+};
+
+const RequestModal = ({ requestNumber, onClose }: Props) => {
+  const handleOnClick = () => {
+    console.log('accept');
+  };
+  return (
+    <div className="border-mediumGreen flex flex-col items-center justify-center border-[1rem] border-solid bg-white">
+      <div className="flex w-full flex-col items-end justify-center">
+        <button
+          className="close hover:text-grey focus:text-grey mr-4 text-[28px] font-bold text-black hover:cursor-pointer"
+          onClick={() => onClose()}
+        >
+          &times;
+        </button>
+        <div className="mt-[-1rem] flex w-full items-center justify-center">
+          <div className="ml-[1rem] flex items-center justify-center pr-[0.5rem]">
+            <Image
+              src="/assets/wave-user.svg"
+              width="24px"
+              height="24px"
+              alt="Request Icon"
+            />
+          </div>
+          <h2 className="py-4 text-[1.5rem]">
+            You have {requestNumber} Mentorship{' '}
+            {requestNumber !== 1 ? 'Requests!' : 'Request!'}
+          </h2>
+        </div>
+      </div>
+      <div className=" bg-sectionPink flex w-full flex-row flex-wrap justify-center px-[1.5rem] py-4">
+        <ProfileCard accept={true} />
+      </div>
+    </div>
+  );
+};
+
+export default RequestModal;
