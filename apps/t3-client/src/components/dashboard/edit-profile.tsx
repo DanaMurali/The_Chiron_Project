ed89155ba49032e48c0e154602cff74459ce12c9
Jordan Smith <jelliesmiff@gmail.com>
2022-11-10 23:18:32 +0000

    form to edit profile, as modal (#71)

---
+++ apps/t3-client/src/components/dashboard/edit-profile.tsx
@@ -0,0 +1,113 @@
+import { FormEvent, useState } from 'react';
+
+type Props = {
+  onClose: () => void;
+};
+
+const EditProfile = ({ onClose }: Props) => {
+  const [roleInput, setRoleInput] = useState('');
+  const [biographyInput, setBiographyInput] = useState('');
+  const [isMentor, setIsMentor] = useState(false);
+  const [isMentee, setIsMentee] = useState(false);
+
+  const handleOnSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(roleInput, biographyInput, isMentor, isMentee, 'submitted');
+    setRoleInput('');
+    setBiographyInput('');
+    setIsMentor(false);
+    setIsMentee(false);
+  };
+
+  return (
+    <div className="border-mediumGreen rounded border-[1rem]">
+      <div className="flex items-center justify-end">
+        <button
+          className="close hover:text-grey focus:text-grey float-right mr-4 text-[28px] font-bold text-black hover:cursor-pointer"
+          onClick={() => onClose()}
+        >
+          &times;
+        </button>
+      </div>
+      <h2 className="py-4  text-center text-[2.5rem] font-bold">
+        Edit your profile:
+      </h2>
+      <div className="bg-sectionPink w-100% mb-[2rem] flex min-h-[20rem] flex-col items-center justify-center border-2">
+        <form onSubmit={(e) => handleOnSubmit(e)}>
+          <div className="mb-2 flex">
+            <label
+              htmlFor="role"
+              className="border-textGrey mr-[1rem] flex w-[11rem] items-center justify-center border bg-white"
+            >
+              Job Role:
+            </label>
+            <input
+              type="text"
+              placeholder="software engineer"
+              id="role"
+              className="form-control border-textGrey m-0 block w-full border border-solid bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+              value={roleInput}
+              onChange={(e) => setRoleInput(e.target.value)}
+            />
+          </div>
+          <div className="mb-2 flex">
+            <label
+              htmlFor="biography"
+              className="border-textGrey mr-[1rem] flex w-[11rem] items-center justify-center border bg-white"
+            >
+              Biography:
+            </label>
+            <input
+              type="text"
+              placeholder="loves typescript"
+              id="biography"
+              className="form-control border-textGrey m-0 block w-full border border-solid bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+              value={biographyInput}
+              onChange={(e) => setBiographyInput(e.target.value)}
+            />
+          </div>
+          <div className="mx-auto flex max-w-[20rem] flex-col items-center justify-center text-center">
+            <label className="border-textGrey mr-[1rem] flex h-[38px] w-[11rem] items-center justify-center border bg-white">
+              I want to be ...
+            </label>
+            <div className="flex">
+              <label htmlFor="mentor" className="mr-2">
+                a mentor:
+              </label>
+              <input
+                type="checkbox"
+                id="mentor"
+                onChange={(e) => setIsMentor(e.target.checked)}
+                checked={isMentor}
+              />
+            </div>
+            <div className="flex">
+              <label htmlFor="mentee" className="mr-2">
+                a mentee:
+              </label>
+              <input
+                type="checkbox"
+                id="mentee"
+                onChange={(e) => setIsMentee(e.target.checked)}
+                checked={isMentee}
+              />
+            </div>
+            <p>
+              note: please select both if you&apos;d like to be both mentor and
+              mentee!
+            </p>
+          </div>
+          <div className="mt-4 flex items-center">
+            <input
+              type="submit"
+              className="bg-blackCoral mx-auto h-11 min-w-[6rem] cursor-pointer rounded-md text-sm text-white sm:w-[174px]"
+              value="UPDATE MY PROFILE"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditProfile;
