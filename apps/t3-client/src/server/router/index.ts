9f5e0d5975ae199a61228a6967d68c81230e644f
JJ <jelliesmiff@gmail.com>
2022-11-11 18:28:15 +0000

    find user mentors query

--- apps/t3-client/src/server/router/index.ts
+++ apps/t3-client/src/server/router/index.ts
@@ -7,7 +7,7 @@ import { createUser } from './createUser';
 import { findMentee } from './findMentee';
 import { findMentor } from './findMentor';
 import { protectedExampleRouter } from './protected-example-router';
-import { findUserAllMentor } from './findUserAllMentors';
+import { findUserMentors } from './findUserMentors';
 
 export const appRouter = createRouter()
   .transformer(superjson)
@@ -17,7 +17,7 @@ export const appRouter = createRouter()
   .merge('findMentee.', findMentee)
   .merge('findMentor.', findMentor)
   .merge('test.', testRouter)
-  .merge('findUserAllMentor.', findUserAllMentor);
+  .merge('findUserAllMentor.', findUserMentors);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
